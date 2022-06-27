4ecd470faa655edaefc9ef1b9c26c18d31bc925a
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-28 00:59:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
+  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(json => renderBooks(json))
+  
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
